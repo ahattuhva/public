31fa4a388c4ef2f9b142975c5d80f3cb744ebee9
ahattuhva <114632235+ahattuhva@users.noreply.github.com>
2022-11-26 14:22:28 +0700

    Update aboba.py

--- aboba.py
+++ aboba.py
@@ -1,21 +1,34 @@
 from PIL import Image, ImageDraw, ImageFont
 
-def f(t):
+def f(t, i):
 
-
-
-
-    im = Image.new('RGB', (200,200), color=('#FAACAC'))
-    font = ImageFont.truetype('C:/Windows/Fonts/impact.ttf', size=20)
+    im = Image.new('RGB', (1920,1080), color=('#59a946'))
+    font = ImageFont.truetype('C:/Windows/Fonts/impact.ttf', size=(100))
+    for i in range (10):
+        im.save('C:/Users/student/Documents/142/хайп/bebra'+str(i)+'.jpg')
+    
     draw_text = ImageDraw.Draw(im)
     draw_text.text(
         (100,100),
-        ' яяяяяяяя ',
+        t,
         font=font,
-        fill=('#1C0606')
+        fill=('#ffffff')
         )
     im.show()
+    im.save('C:/Users/student/Documents/142/хайп/bebra'+str(i)+'.jpg')
 t='яяяяяяяя'
+t=['ВСЁ РЕШЕНО', 
+   'Я ЛИЦЕИСТ',
+   'Я ПОВЗРОСЛЕЛ',
+   'МОЖЕТЕ ПРОСТО ПРОМОЛЧАТЬ',
+   'МОЖЕТЕ ЗЛИТЬСЯ',
+   'ИЛИ БЕСИТЬСЯ',
+   'НУ, А Я ПОШЁЛ УЧИТЬ',
+   'АЛГЕБРУ',
+   'ОБЩЕСТВО',
+   'ГЕОМЕТРИЮ']
+for i in range(10):
+    f(t[i],i)
 f(t)
 
 
